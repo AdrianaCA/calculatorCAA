e8daf1fd1147f205e326fed737ac43e2a6035ac7
AdrianaCA <cabaleiroa78@gmail.com>
2026-05-04 21:30:13 +0200

    feat: Add Checkstyle configuration and static analysis to CI workflow

--- src/main/java/com/dam/calculatorCAA/CalculatorCaaApplication.java
+++ src/main/java/com/dam/calculatorCAA/CalculatorCaaApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CalculatorCaaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CalculatorCaaApplication.class, args);
-	}
+public static void main(String[] args) {
+SpringApplication.run(CalculatorCaaApplication.class, args);
+}
 
 }
